38c098a64dc17ebbbbed90e57d96d761fd5c6ce6
Dukunde Irakoze Happy <acele4444@gmail.com>
2023-08-14 10:31:48 +0200

    teacher's model and controller

---
+++ models/Teacher.js
@@ -0,0 +1,39 @@
+const mongoose = require('mongoose')
+
+const teacherSchema = mongoose.Schema({
+    fullName :{
+        type: String,
+        required: true
+    },
+    email:{
+        type: String,
+        required: true,
+        unique: true
+    },
+    phoneNumber:{
+        type: String,
+        minlength: 10,
+        maxlength: 10
+    },
+    password:{
+        type: String,
+        required: true
+    },
+    course:{
+        type: String,
+        enum:['ECONOMICS','MATHEMATICS','ENGINEERING','CONSTRUCTION'],
+        required:true
+    },
+    startingDate:{
+        type: Date,
+        default: Date.now()
+    },
+    salary:{
+        type: Number,
+        required: true
+    }
+
+})
+
+const Teacher = new mongoose.Model('Teacher',teacherSchema)
+module.exports = Teacher
