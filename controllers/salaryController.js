473130b16fe3b7ede1b4e254f9f6a5bbe9a0507f
Dukunde Irakoze Happy <acele4444@gmail.com>
2023-08-17 23:49:20 +0200

    fixed hod confirm payment

--- controllers/salaryController.js
+++ controllers/salaryController.js
@@ -40,14 +40,47 @@ exports.claimSalary = async(req,res)=>{
 
 }
 
+exports.confirmPaymentCP = async(req,res)=>{
+    const cpId = req.params.id.split(':')[1]
+    const cp = await User.findOne({_id:cpId})
+
+    const teacherId = cp.notifications[cp.notifications.length - 1].from 
+    const teacher = await User.findOne({_id:teacherId})
+    const department = cp.department
+
+    const HOD = await User.findOne({department:department  , role:"HOD"})
+
+    try{
+        const HODNotification = await new Notification({
+            from: cpId,
+            to: HOD._id,
+            content:`Hello HOD ${HOD.fullName.split(' ')[0]}, I just Approved The payment for Teacher ${teacher.fullName.split(' ')[0]} ,who is in my department claiming  ${teacher.salary} Rwf Salary As they finished teaching the ${teacher.course} course !`
+        })
+        
+        const savedNotification = await HODNotification.save();
+        const updatedUser = await User.findByIdAndUpdate(
+            HOD._id,
+            { $push: { notifications: savedNotification._id } },
+            { new: true }
+          );
+        //   console.log('Updated user:', updatedUser);
+          return res.status(204).send(updatedUser)
+        } catch (error) {
+        //   console.error('Error:', error);
+          return res.status(500).send(error);
+        }
+}
+
 
 exports.confirmPaymentHOD = async(req,res)=>{
     const hodId = req.params.id.split(':')[1]
     const hod = await User.findOne({_id:hodId})
-    const teacherId = hod.notifications[hod.notifications.length - 1].from
+    
+    const cpId = hod.notifications[hod.notifications.length - 1].from
+    const cp = await User.findOne({_id:cpId})
 
-    const teacher = await User.findOne({_id:teacherId})
-
+    const teacherId = hod.notifications[hod.notifications.length -1].from
+    const teacher = await User.findOne({_id: teacherId})
     //academic
     const academic = await User.findOne({role:"ACADEMICS"})
     try{
